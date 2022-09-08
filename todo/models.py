3463a37f901dd2669687f3b2172ce75e9d3e0dd6
Alexandru-Andrei Todor <at@interap.de>
2022-09-08 19:05:21 +0200

    todo app deleted

--- todo/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from datetime import datetime
-# Create your models here.
-
-class Todo(models.Model):
-    title = models.CharField(max_length=500)
-    description = models.CharField(max_length=500)
-    created_at = models.DateField(default=datetime.now)
--- todo/serializers.py
+++
@@ -1,8 +0,0 @@
-from .models import Todo
-from rest_framework import serializers
-
-#This build an endpoint for the API to access the data
-class TodoSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Todo
-        fields = ['title', 'description', 'created_at']
